5b8ba39b4109058fbd54a55f75e03c48760de6d4
alfenno17 <alfenno1602@gmail.com>
2026-07-17 15:00:05 +0700

    latihan routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hi! Selamat Datang di Website Laravel';
+});
+
+Route::get('/about', function () {
+    return 'NIS : 25967, Nama : Alfenno Septian, Kelas : XI RPLB';
+});
+
+Route::get('/world', [WelcomeController::class, 'hello']);
+
+Route::get('siswa', function ($id) {});
+Route::post('siswa', function ($id) {});
+Route::put('siswa', function ($id) {});
+Route::delete('siswa', function ($id) {});
+Route::get('siswa/{id}', function ($id) {});
+Route::put('siswa/{id}', function ($id) {});
+Route::delete('siswa/{id}', function ($id) {});
+
+
+Route::get('/articles/{id}', function ($id) {
+    return "Ini adalah halaman Artikel dengan ID: " . $id;
 });
