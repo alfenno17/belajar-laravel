b32ad8ce71654602b115561788039fb1b7df10de
alfenno17 <alfenno1602@gmail.com>
2026-07-31 13:29:04 +0700

    view

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,10 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dashboard')</title>
 
-    <link rel="stylesheet" href="{{ asset('king-of-pasta/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('king-of-pasta/css/screen.css') }}">
-    <link rel="stylesheet" href="{{ asset('king-of-pasta/css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('king-of-pasta/css/jquery.fullPage.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
         body {
